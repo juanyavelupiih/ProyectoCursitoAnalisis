d861cc13cd57d1fe308fe67104dd093104d3a01e
juanyavelupiih <70999821+juanyavelupiih@users.noreply.github.com>
2020-09-16 23:59:56 -0500

    proyecto recuperame

---
+++ Recupereme señor/Recupereme señor/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Recupereme_señor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(413, 28);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Datos1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(417, 335);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(856, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Graficar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(205, 93);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(177, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(205, 148);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(177, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(25, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pais";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Datos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Archivo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(437, 399);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Promedio";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 210);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(586, 404);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(230, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(28, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(354, 68);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 470);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Recupereme señor/Recupereme señor/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Recupereme_señor
+{
+    public partial class Form1 : Form
+    {
+        /*Atributos (variables globales)*/
+        // Crear una lista para almacenar los encabezados
+        List<string> encabezado = new List<string>();
+        // Crear una lista para almacenar los años
+        List<string> pais = new List<string>();
+        // Lista para guardar todos los datos
+        List<string[]> data = new List<string[]>();
+
+        // string[] arch = {"0"};
+
+        public void datos()
+        {
+            /* // Variables locales
+			List<string> tmp = new List<string>();
+			
+			int maximo = int.MinValue;
+			int minimo = int.MaxValue;
+			long total = 0;
+			double promedio = 0.0;
+			
+			int i = comboBox1.SelectedIndex + 1;
+			int j = comboBox2.SelectedIndex;
+			
+			// Obtener todos los datos de la selección
+			foreach(string[] r in data)
+			{
+				// Añadir los valores a la lista temporal
+				tmp.Add(r[i]);
+				
+				// Obtener la suma de todos los valores
+				total += Convert.ToInt32(r[i]);
+				
+				// Obtener máximo de la lista
+				if( Convert.ToInt32(r[i]) > maximo )
+					maximo = Convert.ToInt32(r[i]);
+				// Obtener el mínimo de la lista
+				if( Convert.ToInt32(r[i]) < minimo )
+					minimo = Convert.ToInt32(r[i]);
+				
+			}
+			
+			// Obtenemos el promedio
+			promedio = (double)total / (double)tmp.Count;
+			
+			if(comboBox2.SelectedIndex != -1)
+				textBox2.Text = tmp[j];
+			
+			textBox2.Text = promedio.ToString();
+			
+			*/
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rdn = new Random();
+
+            // Limpiar los puntos de los datos
+            chart1.Series["Datos1"].Points.Clear();
+
+
+            // Cambiar el texto de la leyenda
+            chart1.Series["Datos1"].LegendText = "Puntos Aleatorios 1";
+
+
+            chart1.Series["Datos1"].IsValueShownAsLabel = true;
+
+
+            // Añadir elementos a la gráfica
+            for (int i = 0; i < 10; i++)
+            {
+                chart1.Series["Datos1"].Points.AddXY(rdn.Next(0, 10), rdn.Next(0, 10));
+
+            }
+
+            chart1.Series["Datos1"].ChartType = SeriesChartType.BoxPlot;
+            chart1.Series["Datos1"].Color = Color.Red;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Añadir un título a la gráfica
+            chart1.Titles.Add("Grafica Recuperame");
+            // Modificar el fondo del título
+            chart1.Titles[0].BackColor = Color.Coral;
+
+            // Dar formato a la leyenda
+            chart1.Legends.Add("Leyenda1");
+            chart1.Legends[0].BackColor = Color.BlueViolet;
+            chart1.Legends[0].BorderColor = Color.GreenYellow;
+            chart1.Legends[0].BorderWidth = 4;
+
+            // Textos en los ejes
+            chart1.ChartAreas[0].Axes[0].Title = "Eje x";
+            chart1.ChartAreas[0].Axes[1].Title = "Eje y";
+
+            // Variables locales
+            int j = 0;      // Contador de líneas
+
+            // Lectura del archivo CSV
+            using (var lector = new StreamReader("owid-covid-data.csv"))
+            {
+                // Hasta el fin del archivo
+                while (!lector.EndOfStream)
+                {
+                    // Leer línea por línea ('\n')
+                    var linia = lector.ReadLine();
+
+                    // Encabezado
+                    if (j == 0)
+                    {
+                        // Recorrer elemento a elemento y añadir a una lista
+                        foreach (string en in linia.Split(','))
+                            encabezado.Add(en);
+                    }
+                    else
+                    {
+                        // Añadir los elementos de la tabla
+                        data.Add(linia.Split(','));
+                        // Se añaden los paises
+                        pais.Add(linia.Split(',')[2]);
+                    }
+                    // contador de líneas leídas
+                    j++;
+                }
+
+                /* Leídos los datos, comenzar a llenar los espacios de la GUI */
+                encabezado.RemoveRange(0, 1); encabezado.RemoveRange(0, 3);
+                foreach (string en in encabezado)
+                    comboBox2.Items.Add(en);
+                comboBox2.Text = comboBox2.Items[0].ToString();
+
+                // Colocar los paises
+                foreach (string p in pais)
+                    comboBox1.Items.Add(p);
+                comboBox1.Text = comboBox1.Items[2].ToString();
+
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //datos();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //datos();
+        }
+    }
+}
